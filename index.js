fc636f1eca3d301ed78b77cd3a8117a7107bf32b
Martin Giger <martin@humanoids.be>
2016-09-07 04:51:07 +0200

    Some perf analytics

--- index.js
+++ index.js
@@ -35,24 +35,18 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 const letters = alphabet.split('');
 const minOccurences = 3;
 
+let wastedTime = 0;
+let emptyCount = 0;
+let wordCount = 0;
+
 getBots().then((bots) => {
-    const findings = new Map();
     const names = bots.map((b) => b.username).filter((n) => n.length >= minLength);
-
-    // Two bots can't have the same name, so at least a letter is probably different.
-    const maxLength = Math.max.apply(Math, names.map((n) => n.length)) - 1;
-
-    console.log("Looking for words between", colors.blue(minLength), "and", colors.blue(maxLength), "characters.");
 
     info("Generating base words");
 
     const foundWordsByLength = [ null, null ];
 
     const eligibleNames = {};
-    let futureNames = [],
-        words = [],
-        futureWords = [],
-        wordLength = 1;
 
     names.forEach((n) => {
         letters.forEach((l) => {
@@ -69,23 +63,31 @@ getBots().then((bots) => {
 
     info("Analysing word frequency");
 
+    let futureNames = [],
+        words = Object.keys(eligibleNames),
+        futureWords = [],
+        wordLength = 1,
+        word;
+
     const filterEligibleNames = (word, n) => n.includes(word);
     const lttrs = (b, letter) => {
-        const word = b + letter;
+        ++wordCount;
+        const s = Date.now();
+        word = b + letter;
         futureNames = eligibleNames[b].filter(filterEligibleNames.bind(null, word));
         if(futureNames.length > minOccurences) {
-            if(wordLength >= minLength) {
-                findings.set(word, futureNames.length);
-            }
             futureWords.push(word);
             eligibleNames[word] = futureNames;
+        }
+        else if(futureNames.length == 0 && word.length >= 4) {
+            wastedTime += Date.now() - s;
+            ++emptyCount;
         }
     };
     const wrds = (b) => {
         letters.forEach(lttrs.bind(null, b));
     };
 
-    words = Object.keys(eligibleNames);
     while(words.length > 0) {
         futureWords = [];
         ++wordLength;
@@ -103,18 +105,21 @@ getBots().then((bots) => {
 
     // Take out shorter strings with same occurence count as longer string that
     // they are a substring of.
-
-    info("Removing shorter duplicates");
-    for(let w = minLength + 1; w <= maxLength && Array.isArray(foundWordsByLength[w]) && foundWordsByLength[w].length > 0; ++w) {
-        const prev = foundWordsByLength[w - 1];
-        const curr = foundWordsByLength[w];
-
-        for(const word of prev) {
-            const longerWord = curr.find((a) => a.includes(word));
-            if(findings.get(word) == findings.get(longerWord)) {
-                findings.delete(word);
-            }
+    info("Building duplicate free dictionary");
+    const findings = new Map();
+    let l, prev, curr, longerWord;
+    const prevSearch = (word) => {
+        longerWord = curr.find((a) => a.includes(word));
+        l = eligibleNames[word].length;
+        if(!longerWord || l != eligibleNames[longerWord].length) {
+            findings.set(word, l);
         }
+    };
+    for(let w = minLength + 1; w < foundWordsByLength.length && Array.isArray(foundWordsByLength[w]) && foundWordsByLength[w].length > 0; ++w) {
+        prev = foundWordsByLength[w - 1];
+        curr = foundWordsByLength[w];
+
+        prev.forEach(prevSearch);
     }
 
     info("Sorting results");
@@ -125,8 +130,14 @@ getBots().then((bots) => {
         head: [ "Word", "Count", "Percentage" ]
     });
 
-    table.push.apply(table, sortedResults.map((n) => [ n, findings.get(n), ((findings.get(n) / bots.length) * 100).toFixed(2) + "%" ]));
+    table.push.apply(table, sortedResults.map((n) => [
+        n,
+        findings.get(n),
+        ((findings.get(n) / bots.length) * 100).toFixed(2) + "%"
+    ]));
 
     console.log(table.toString());
     console.log("And it only took", colors.blue(((Date.now() - start) / 1000).toFixed(2)), "seconds to get here");
+    console.log(colors.blue((wastedTime/1000).toFixed(2)), "ms wasted on unued letters, that's", colors.green((wastedTime/(Date.now() - start)*100)+"%"));
+    console.log((emptyCount/wordCount*100).toFixed(2)+"%");
 });
