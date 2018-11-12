13cbd475a1faa13f41b9ff0e8d369b88f6103863
Martin Giger <martin@humanoids.be>
2018-11-12 19:39:44 +0100

    Convert to listr because that's much nicer output management

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const twitchbots = require("twitchbots-node"),
     Table = require("cli-table"),
     chalk = require("chalk"),
+    Listr = require("listr"),
     util = require("util"),
     fs = require("fs"),
     getOccurences = require("./count-occurences"),
     cacheFile = "bots.json",
-    info = (str) => console.log(chalk.grey(`${str}...`)),
     /* eslint-disable security/detect-non-literal-fs-filename */
     fsExists = util.promisify(fs.access),
     fsReadFile = util.promisify(fs.readFile),
@@ -20,59 +20,77 @@ const twitchbots = require("twitchbots-node"),
     TO_PERCENT = 100,
     MS_TO_S = 1000,
     PRECISION = 2,
-    /**
-     * @returns {[string]} All bot usernames on twitchbots.info.
-     */
-    getBots = async () => {
-        let noError = true;
-        try {
-            await fsExists(cacheFile, fs.constants.F_OK);
+    tasks = new Listr([
+        {
+            title: "Check cache",
+            task: async (ctx) => {
+                ctx.isCached = true;
+                try {
+                    await fsExists(cacheFile, fs.constants.F_OK);
+                }
+                catch(error) {
+                    ctx.isCached = false;
+                }
+            }
+        },
+        {
+            title: "Loading all bots from cached file",
+            enabled: (ctx) => ctx.isCached,
+            task: async (ctx) => {
+                const contents = await fsReadFile(cacheFile, ENCODING);
+                ctx.bots = JSON.parse(contents);
+            }
+        },
+        {
+            title: "Fetching from the twitchbots.info API",
+            enabled: (ctx) => !ctx.isCached,
+            task: async (ctx) => {
+                const bots = await twitchbots.getAllBots();
+                ctx.bots = bots.map((b) => b.username);
+            }
+        },
+        {
+            title: "Caching bots to disk",
+            enabled: (ctx) => !ctx.isCached,
+            task: (ctx) => fsWriteFile(cacheFile, JSON.stringify(ctx.bots), ENCODING)
+        },
+        {
+            title: "Analyzing word frequency",
+            task: (ctx) => {
+                ctx.time = Date.now();
+                ctx.findings = getOccurences(ctx.bots, BOT_COUNT);
+                ctx.time = Date.now() - ctx.time;
+            }
+        },
+        {
+            title: "Sorting results",
+            task: (ctx) => {
+                ctx.sortedResults = Object.keys(ctx.findings).sort((a, b) => ctx.findings[b] - ctx.findings[a])
+                    .slice(START, RESULT_COUNT);
+            }
+        },
+        {
+            title: "Generating nice output table",
+            task: (ctx, task) => {
+                const table = new Table({
+                    head: [
+                        "Word",
+                        "Count",
+                        "Percentage"
+                    ]
+                });
+
+                table.push(...ctx.sortedResults.map((n) => [
+                    n,
+                    ctx.findings[n],
+                    `${((ctx.findings[n] / ctx.bots.length) * TO_PERCENT).toFixed(PRECISION)}%`
+                ]));
+                ctx.table = table.toString();
+            }
         }
-        catch(error) {
-            noError = false;
-        }
-        if(noError) {
-            info("Loading from cached file");
-            const contents = await fsReadFile(cacheFile, ENCODING);
-            return JSON.parse(contents);
-        }
+    ]);
 
-        info("Fetching from the twitchbots.info API");
-        const bots = await twitchbots.getAllBots(),
-            names = bots.map((b) => b.username);
-        info("Caching it to disk");
-        await fsWriteFile(cacheFile, JSON.stringify(names), ENCODING);
-        return names;
-    };
-
-info("Loading all bots from twitchbots.info");
-
-getBots().then((bots) => {
-    info("Analyzing word frequency");
-    let time = Date.now();
-    const findings = getOccurences(bots, BOT_COUNT);
-    time = Date.now() - time;
-
-    info("Sorting results");
-    const sortedResults = Object.keys(findings).sort((a, b) => findings[b] - findings[a])
-        .slice(START, RESULT_COUNT);
-
-    info("Generating nice output table");
-    const table = new Table({
-        head: [
-            "Word",
-            "Count",
-            "Percentage"
-        ]
-    });
-
-    table.push(...sortedResults.map((n) => [
-        n,
-        findings[n],
-        `${((findings[n] / bots.length) * TO_PERCENT).toFixed(PRECISION)}%`
-    ]));
-
-    console.log(table.toString());
-    console.log("And it only took", chalk.blue((time / MS_TO_S).toFixed(PRECISION)), "seconds to get here");
-})
-    .catch((e) => console.warn(e));
+tasks.run().then((ctx) => {
+    console.log(ctx.table);
+    console.log(chalk`And it only took {blue ${(ctx.time / MS_TO_S).toFixed(PRECISION)}} seconds to get here`);
+}).catch(console.error);
